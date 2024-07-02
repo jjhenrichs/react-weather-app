e6ef5a65a6ae81214231a9497d691f0beaca31b5
Jeff Henrichs <jeffhenrichs37@gmail.com>
2024-07-02 16:37:40 -0400

    Able to retrive coords from locationInfo and display either coords or an error message

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useGeolocation from "../hooks/useGeolocation";
 
 function App() {
   const [locationInfo, locationError] = useGeolocation();
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  useEffect(() => {
+    const data = document.getElementById("data");
+
+    if (locationError) {
+      console.log(locationError);
+      data.innerHTML = `${locationError}`;
+    }
+
+    if (locationInfo) {
+      setLatitude(locationInfo.latitude);
+      setLongitude(locationInfo.longitude);
+      data.innerHTML = `Latitude: ${latitude}, Longitude: ${longitude}`;
+    }
+  }, [locationError, locationInfo, latitude, longitude]);
 
   return (
     <div className="App">
       <h1 className="App-header"> Weather App</h1>
-      {console.log(locationInfo, locationError)}
+      <div id="data"></div>
     </div>
   );
 }
