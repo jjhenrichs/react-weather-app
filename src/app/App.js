b58329183dbff21dd816001220989603245e8276
Jeff Henrichs <jeffhenrichs37@gmail.com>
2024-07-02 15:06:35 -0400

    Able to retrieve coordinates with Geolocation API

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(successCallBack, errorCallBack);
+  }, []);
+
+  const successCallBack = (position) => {
+    console.log(position);
+  };
+
+  const errorCallBack = (error) => {
+    console.log(error);
+  };
+
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
